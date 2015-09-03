f1b3f166368d69c0622ae2066359a0a0a51e2527
Mario-S <spindizzy@outlook.com>
2015-09-03 15:15:35 +0200

    added exception handling for invalid command

--- src/main/java/io/atom/electron/RunAction.java
+++ src/main/java/io/atom/electron/RunAction.java
@@ -3,17 +3,22 @@ package io.atom.electron;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
+import java.util.concurrent.Future;
+import javax.swing.Icon;
 import org.netbeans.api.extexecution.ExecutionDescriptor;
 import org.netbeans.api.extexecution.ExecutionService;
 import org.netbeans.api.extexecution.ProcessBuilder;
 import org.openide.DialogDisplayer;
+import org.openide.ErrorManager;
 import org.openide.NotifyDescriptor;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.awt.NotificationDisplayer;
 import org.openide.filesystems.FileUtil;
+import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
 import org.openide.util.NbBundle.Messages;
 
@@ -27,15 +32,17 @@ import org.openide.util.NbBundle.Messages;
 @ActionReference(path = "Loaders/text/javascript/Actions", position = 0)
 @Messages("CTL_LaunchAction=Run with Electron")
 public final class RunAction implements ActionListener {
+
     private static final String ELECTRON = "electron";
+    private static final String ELECTRON_ICON = "io/atom/electron/atom_24x24.png";
 
     private final DataObject context;
 
     private final ExecutionDescriptor descriptor;
-    
+
     public RunAction(DataObject context) {
         this.context = context;
-        
+
         descriptor = new ExecutionDescriptor()
                 .frontWindow(true)
                 .frontWindowOnError(true)
@@ -47,34 +54,50 @@ public final class RunAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent ev) {
         String cmd = PreferencesAccess.Instance.getExe();
-        if(cmd == null){
-            showError();
-        }else{
+        if (cmd == null) {
+            showError(getMessage("exe.missing"));
+        } else {
             launch(cmd);
         }
     }
 
-    private void showError() {
-        String msg = NbBundle.getMessage(getClass(), "exe.missing");
+    private String getMessage(String key) {
+        return NbBundle.getMessage(getClass(), key);
+    }
+
+    private void showError(String msg) {
         NotifyDescriptor notifyDescr = new NotifyDescriptor.Message(msg, NotifyDescriptor.ERROR_MESSAGE);
         DialogDisplayer.getDefault().notify(notifyDescr);
+    }
+
+    private void notify(Exception e) {
+        Icon icon = ImageUtilities.loadImageIcon(ELECTRON_ICON, true);
+        NotificationDisplayer noti = NotificationDisplayer.getDefault();
+        noti.notify(getMessage("exc"), icon, e.getLocalizedMessage(), this,
+                NotificationDisplayer.Priority.HIGH, NotificationDisplayer.Category.ERROR);
+        ErrorManager.getDefault().notify(e);
     }
 
     private void launch(String cmd) {
         ProcessBuilder processBuilder = createProcessBuilder(cmd);
         ExecutionService service = ExecutionService.newService(processBuilder, descriptor, ELECTRON);
-        service.run();
+        try {
+            Future<Integer> task = service.run();
+            task.get();
+        } catch (Exception e) {
+            notify(e);
+        }
     }
 
     private ProcessBuilder createProcessBuilder(String cmd) {
         FileObject fo = context.getPrimaryFile();
-        String [] args = new String[]{FileUtil.getFileDisplayName(fo)};
-        
+        String[] args = new String[]{FileUtil.getFileDisplayName(fo)};
+
         ProcessBuilder processBuilder = ProcessBuilder.getLocal();
         processBuilder.setExecutable(cmd);
         processBuilder.setArguments(Arrays.asList(args));
         processBuilder.setRedirectErrorStream(true);
-        
+
         return processBuilder;
     }
 }
