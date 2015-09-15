ea7a84b2abef313b14b35a493aae5d5726d1739c
Mario-S <spindizzy@outlook.com>
2015-09-15 20:32:18 +0200

    update panel with hint for node-inspector

--- src/main/java/io/atom/electron/ElectronPanel.java
+++ src/main/java/io/atom/electron/ElectronPanel.java
@@ -1,11 +1,16 @@
 package io.atom.electron;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Arrays;
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 
 final class ElectronPanel extends javax.swing.JPanel {
@@ -13,7 +18,7 @@ final class ElectronPanel extends javax.swing.JPanel {
     private final String invalidPathMessage;
 
     private final ElectronOptionsPanelController controller;
-    
+
     private final ElectronPreferences prefs;
 
     ElectronPanel(ElectronOptionsPanelController controller) {
@@ -42,6 +47,9 @@ final class ElectronPanel extends javax.swing.JPanel {
         lblDebugPort = new javax.swing.JLabel();
         txtDebugPort = new javax.swing.JTextField();
         chkPause = new javax.swing.JCheckBox();
+        jSeparator1 = new javax.swing.JSeparator();
+        lblReq = new javax.swing.JLabel();
+        lblNodeInspec = new javax.swing.JLabel();
 
         lblPath.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         org.openide.awt.Mnemonics.setLocalizedText(lblPath, org.openide.util.NbBundle.getMessage(ElectronPanel.class, "ElectronPanel.lblPath.text")); // NOI18N
@@ -76,29 +84,48 @@ final class ElectronPanel extends javax.swing.JPanel {
             }
         });
 
+        org.openide.awt.Mnemonics.setLocalizedText(lblReq, org.openide.util.NbBundle.getMessage(ElectronPanel.class, "ElectronPanel.lblReq.text")); // NOI18N
+
+        lblNodeInspec.setForeground(new java.awt.Color(0, 0, 204));
+        org.openide.awt.Mnemonics.setLocalizedText(lblNodeInspec, org.openide.util.NbBundle.getMessage(ElectronPanel.class, "ElectronPanel.lblNodeInspec.text")); // NOI18N
+        lblNodeInspec.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblNodeInspec.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblNodeInspecMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblPath, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblDebugPort, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblCmd, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jSeparator1)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(chkPause)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(lblPath, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblDebugPort, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblCmd, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(errLblPath, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(txtPath, javax.swing.GroupLayout.DEFAULT_SIZE, 193, Short.MAX_VALUE)
-                            .addComponent(txtRunCmd)
-                            .addComponent(txtDebugPort))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnBrowse)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(chkPause)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(lblReq)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(lblNodeInspec)
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addComponent(errLblPath, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(txtPath, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 193, Short.MAX_VALUE)
+                                    .addComponent(txtRunCmd, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtDebugPort, javax.swing.GroupLayout.Alignment.LEADING))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnBrowse)))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -115,13 +142,19 @@ final class ElectronPanel extends javax.swing.JPanel {
                     .addComponent(btnBrowse))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(errLblPath, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGap(1, 1, 1)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblReq)
+                    .addComponent(lblNodeInspec))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblDebugPort)
                     .addComponent(txtDebugPort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(chkPause)
-                .addContainerGap(72, Short.MAX_VALUE))
+                .addContainerGap(50, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -137,6 +170,18 @@ final class ElectronPanel extends javax.swing.JPanel {
     private void chkPauseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chkPauseActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_chkPauseActionPerformed
+
+    private void lblNodeInspecMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblNodeInspecMouseClicked
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
+                URL url = new URL("https://github.com/node-inspector/node-inspector#quick-start");
+                desktop.browse(url.toURI());
+            } catch (IOException | URISyntaxException e) {
+                Exceptions.printStackTrace(e);
+            }
+        }
+    }//GEN-LAST:event_lblNodeInspecMouseClicked
 
     void load() {
         StringBuilder builder = new StringBuilder();
@@ -173,9 +218,12 @@ final class ElectronPanel extends javax.swing.JPanel {
     private javax.swing.JButton btnBrowse;
     private javax.swing.JCheckBox chkPause;
     private javax.swing.JLabel errLblPath;
+    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel lblCmd;
     private javax.swing.JLabel lblDebugPort;
+    private javax.swing.JLabel lblNodeInspec;
     private javax.swing.JLabel lblPath;
+    private javax.swing.JLabel lblReq;
     private javax.swing.JTextField txtDebugPort;
     private javax.swing.JTextField txtPath;
     private javax.swing.JTextField txtRunCmd;
